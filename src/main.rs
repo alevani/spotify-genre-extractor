58df900bfa0bcc2fafe01086bd40bfc2222b93ef
alevani <vanini.alexandre@gmail.com>
2023-08-13 21:56:05 +0200

    Collect and Hashmap <Genre, track_ids> for all saved music

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::collections::HashMap;
+
+use futures_util::StreamExt;
 use rspotify::{
-    model::TimeRange, prelude::*, scopes, AuthCodeSpotify, Config, Credentials, OAuth, Token,
+    model::{ArtistId, SavedTrack, TimeRange, TrackId},
+    prelude::*,
+    scopes, AuthCodeSpotify, Config, Credentials, OAuth, Token,
 };
 
 #[tokio::main]
@@ -14,11 +19,57 @@ async fn main() {
     // This function requires the `cli` feature enabled.
     spotify.prompt_for_token(&url).await.unwrap();
 
-    // Pauses devices as proof the connection works.
-    for device in spotify.device().await.unwrap().into_iter() {
-        let id = device.id;
-        spotify.pause_playback(Some(&id.unwrap())).await.unwrap();
+    // todo use streams, as this is awefully slow
+    let all_saved_tracks = spotify
+        .current_user_saved_tracks(None)
+        .take(200) // todo to delete
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .filter(|track| track.is_ok());
+
+    let mut genre_tracks: HashMap<String, Vec<TrackId<'static>>> = HashMap::new();
+
+    // Collect and save a list of all genre for each track's artists into a Hashmap
+    for track_data in all_saved_tracks.into_iter() {
+        let track = track_data
+        .unwrap()
+        .track;
+
+        let track_id = track.id.unwrap();
+
+        let taid = track
+            .artists
+            .first()
+            .unwrap()
+            .id
+            .clone()
+            .unwrap();
+        let genres = spotify.artist(taid).await.unwrap().genres;
+        
+        for genre in genres {
+            genre_tracks.entry(genre).or_insert(Vec::new()).push(track_id.clone());
+        }
     }
+
+    println!("{genre_tracks:?}");
+
+    // stream
+    //     .try_for_each_concurrent(10, |item| async move {
+
+    //         // let taid = item.track.artists.first().unwrap().id.unwrap();
+    //         if let Some(artist) = item.track.artists.first().take() {
+    //             taids.push(artist.id.unwrap());
+    //         }
+
+    //         // if let Some(id) = taid {
+    //         //     println!("{:?}", spotify.artist(ids).await.unwrap().genres);
+    //         // }
+
+    //         Ok(())
+    //     })
+    //     .await
+    //     .unwrap();
 }
 
 fn init_spotify() -> AuthCodeSpotify {
