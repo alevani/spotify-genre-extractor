9b7d9759017a126af607068da8afceb031868cd7
alevani <vanini.alexandre@gmail.com>
2023-12-03 20:28:43 +0100

    Sort of added cache and better file struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,131 @@
-use std::io;
-use std::ops::Deref;
+use serde::{Deserialize, Serialize};
+use serde_json::error;
+use std::fs::{create_dir, File};
+use std::io::{BufReader, BufWriter};
 use std::{collections::HashMap, sync::Arc};
 use tokio::time::{sleep, Duration};
 
-use futures_retry::{RetryPolicy, StreamRetryExt};
-
 use futures_util::lock::Mutex;
 use futures_util::StreamExt;
-use rspotify::model::ArtistId;
+use rspotify::model::{ArtistId, FullTrack};
 use rspotify::{model::TrackId, prelude::*, scopes, AuthCodeSpotify, Config, Credentials, OAuth};
+
+fn load_saved_data() -> Vec<Artist> {
+    let mut current_path = dirs::cache_dir().expect("Failed to locate cache dir");
+    current_path.push("playlist-maker");
+
+    if !current_path.as_path().exists() {
+        create_dir(current_path.as_path()).expect("Failed to create cachedir");
+    }
+
+    current_path.set_file_name("metadata.json");
+
+    let f = File::open(current_path).expect("Failed to open cached OAuth2 File");
+    let reader = BufReader::new(f);
+
+    let artists: Result<Vec<Artist>, error::Error> = serde_json::from_reader(reader);
+    artists.unwrap()
+}
+
+fn save_data(artists: &[Artist]) {
+    let mut current_path = dirs::cache_dir().expect("Failed to locate cache dir");
+    current_path.push("playlist-maker");
+
+    if !current_path.as_path().exists() {
+        create_dir(current_path.as_path()).expect("Failed to create cachedir");
+    }
+
+    current_path.set_file_name("metadata.json");
+
+    let f = File::create(current_path).expect("Failed to create metadata.json file");
+    let writer = BufWriter::new(f);
+
+    serde_json::to_writer(writer, artists).expect("Failed to write to metadata.json");
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Artist {
+    pub id: ArtistId<'static>,
+    pub genres: Vec<String>,
+    pub tracks: Vec<FullTrack>,
+}
+
+impl Artist {
+    pub fn set_genres(&mut self, genres: Vec<String>) {
+        self.genres = genres;
+    }
+}
+
+async fn get_metadata(should_sync: bool, spotify: &AuthCodeSpotify) -> Vec<Artist> {
+    if should_sync {
+        // Create a shared state for genre_tracks
+        let artistsids_and_tracks: Arc<Mutex<HashMap<ArtistId, Vec<FullTrack>>>> =
+            Arc::new(Mutex::new(HashMap::new()));
+
+        println!("Fetching current saved tracks ..");
+        spotify
+            .current_user_saved_tracks(None)
+            .for_each_concurrent(None, |t| {
+                print!(".");
+                let artistsids_and_tracks = Arc::clone(&artistsids_and_tracks); // Clone the Arc for the shared state
+
+                async move {
+                    if let Ok(track_data) = t {
+                        let track = track_data.track;
+                        let artist_id = track.artists.first().unwrap().id.clone().unwrap();
+
+                        artistsids_and_tracks
+                            .lock()
+                            .await
+                            .entry(artist_id)
+                            .or_default()
+                            .push(track);
+                    }
+                }
+            })
+            .await;
+
+        let artistsids_and_tracks_locked = artistsids_and_tracks.lock().await;
+
+        let mut artists: Vec<Artist> = artistsids_and_tracks_locked
+            .iter()
+            .map(|e| Artist {
+                id: e.0.clone(),
+                genres: Vec::new(),
+                tracks: e.1.clone(),
+            })
+            .collect();
+
+        println!("\nCurrent saved tracks fetched.");
+        println!(
+            "Retrieving genre by artist for {} artists ..",
+            artistsids_and_tracks_locked.keys().count()
+        );
+
+        let n_artists = artists.len();
+        for (index, artist) in artists.iter_mut().enumerate() {
+            println!("[{index}/{n_artists}] Artist: {}", artist.id);
+
+            let genres = fetch_artist_genres_with_retry(spotify, &artist.id).await;
+            let genres_iter = genres.iter();
+
+            if genres_iter.clone().count() == 0 {
+                println!(" - unknown genre");
+                artist.set_genres(vec!["unknown genre".to_string()]);
+            } else {
+                genres.iter().for_each(|g| println!("{g}"));
+                artist.set_genres(genres);
+            }
+        }
+
+        println!(".. Done !");
+
+        save_data(&artists);
+        artists
+    } else {
+        load_saved_data()
+    }
+}
 
 async fn fetch_artist_genres_with_retry(
     spotify: &AuthCodeSpotify,
@@ -38,72 +155,18 @@ async fn main() {
     // This function requires the `cli` feature enabled.
     spotify.prompt_for_token(&url).await.unwrap();
 
-    // Create a shared state for genre_tracks
-    let mut genre_tracks: HashMap<String, Vec<TrackId>> = HashMap::new();
+    let artists = get_metadata(false, &spotify).await;
 
-    // Create a shared state for genre_tracks
-    let artist_ids: Arc<Mutex<HashMap<ArtistId, Vec<TrackId>>>> =
-        Arc::new(Mutex::new(HashMap::new()));
+    let mut genre_tracks: HashMap<String, Vec<FullTrack>> = HashMap::new();
 
-    println!("Fetching current saved tracks ..");
-    spotify
-        .current_user_saved_tracks(None)
-        .for_each_concurrent(None, |t| {
-            print!(".");
-            let artist_ids = Arc::clone(&artist_ids); // Clone the Arc for the shared state
-
-            async move {
-                if let Ok(track_data) = t {
-                    let track = track_data.track;
-                    let track_id = track.id.unwrap();
-                    let artist_id = track.artists.first().unwrap().id.clone().unwrap();
-
-                    artist_ids
-                        .lock()
-                        .await
-                        .entry(artist_id)
-                        .or_default()
-                        .push(track_id);
-                }
-            }
-        })
-        .await;
-
-    let artist_ids_locked = artist_ids.lock().await;
-
-    println!("\nCurrent saved tracks fetched.");
-    println!(
-        "Retrieving genre by artist for {} artists ..",
-        artist_ids_locked.keys().count()
-    );
-
-    for (index, (aid, vtid)) in artist_ids_locked.iter().enumerate() {
-        println!(
-            "[{index}/{}] Artist: {aid}",
-            artist_ids_locked.keys().count()
-        );
-
-        let genre = fetch_artist_genres_with_retry(&spotify, aid).await;
-        let genre_iter = genre.iter();
-
-        if genre_iter.count() == 0 {
-            println!(" - Unknown genre");
+    artists.iter().for_each(|a| {
+        for genre in a.genres.clone() {
             genre_tracks
-                .entry("unknown genre".to_string())
+                .entry(genre)
                 .or_default()
-                .append(&mut vtid.clone())
-        } else {
-            for genre in fetch_artist_genres_with_retry(&spotify, aid).await {
-                println!(" - {genre}");
-                genre_tracks
-                    .entry(genre)
-                    .or_default()
-                    .append(&mut vtid.clone())
-            }
+                .append(&mut a.tracks.clone());
         }
-    }
-
-    println!(".. Done !");
+    });
 
     for genres in genre_tracks.iter() {
         println!(
@@ -115,7 +178,7 @@ async fn main() {
 
     // let mut input = String::new();
     // let _ = io::stdin().read_line(&mut input);
-    let input = "french indie pop".to_string();
+    let input = "danish pop".to_string();
 
     let user_id = spotify.me().await.unwrap().id;
     let playlist = spotify
@@ -134,18 +197,21 @@ async fn main() {
     let mut tracks: Vec<Vec<PlayableId>> = Vec::new();
     let mut track_record: Vec<PlayableId> = Vec::new();
 
-    for track_id in genre_tracks.get(&input).unwrap() {
+    for track in genre_tracks.get(&input).unwrap() {
         if track_count == 99 {
             track_count = 0;
             tracks.push(track_record);
             track_record = Vec::new();
         }
 
-        track_record.push(PlayableId::Track(track_id.clone()));
+        track_record.push(PlayableId::Track(track.id.as_ref().unwrap().clone()));
         track_count += 1;
     }
 
+    tracks.push(track_record);
+
     for chunk in tracks {
+        println!("Adding tracks to the playlist..");
         let _ = spotify
             .playlist_add_items(playlist.id.clone(), chunk, None)
             .await;
